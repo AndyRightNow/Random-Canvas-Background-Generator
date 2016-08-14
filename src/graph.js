52b3cf102a7a1ef160a431a11a5055b2622b9e01
AndyRightNow <naruto950726@hotmail.com>
2016-08-14 12:39:57 +0800

    Add Graph.connect isConnected disconnect

--- src/graph.js
+++ src/graph.js
@@ -7,12 +7,137 @@
 function Graph(rowCount, columnCount) {
     this._rowCount = rowCount || 0;
     this._columnCount = columnCount || 0;
-    
+
+    //  Allocate an empty matrix
     this._data = (new Array(rowCount)).fill(new Array(columnCount), 0);
+
+    this._edges = {};
 }
 
-Graph.prototype.insert = function(i, j) {
+/*
+ * Private member function - check if a pair of positions is in the range of rows and columns
+ *
+ * @param {Integer} i: The zero-based row position
+ * @param {Integer} j: The zero-based column position
+ */
+Graph.prototype._checkBound = function(i, j) {
+    if (i >= this._rowCount ||
+        j >= this._columnCount ||
+        i < 0 || j < 0) return false;
+    return true;
+};
 
+/*
+ * Private member function - get an id from a pair of positions
+ *
+ * @param {Integer} i: The zero-based row position
+ * @param {Integer} j: The zero-based column position
+ */
+Graph.prototype._getId = function(i, j) {
+    return this._checkBound(i, j) ? i.toString() + j.toString() : null;
+};
+
+/*
+ * Public member function - return the count of rows
+ */
+Graph.prototype.rowCount = function() {
+    return this._rowCount;
+};
+/*
+ * Public member function - return the count of columns
+ */
+Graph.prototype.columnCount = function() {
+    return this._columnCount;
+};
+
+/*
+ * Public member function - insert an element to the graph
+ *
+ * @return {Boolean} true if insertion is successful and false if not
+ * @param {Integer} i: The zero-based row position
+ * @param {Integer} j: The zero-based column position
+ * @param {Any} value: The value to insert
+ */
+Graph.prototype.insert = function(i, j, value) {
+    if (this._checkBound(i, j)) {
+        this._data[i][j] = value;
+        return true;
+    }
+    else return false;
+};
+
+/*
+ * Public member function - get a element from a pair of position
+ *
+ * @param {Integer} i: The zero-based row position
+ * @param {Integer} j: The zero-based column position
+ */
+Graph.prototype.get = function(i, j) {
+    if (this._checkBound(i, j)) {
+        return this._data[i][j];
+    }
+    else return null;
+};
+
+/*
+ * Public member function - check if two vertices are connected
+ *
+ * @param {Integer} i1, i2: The zero-based row position
+ * @param {Integer} j1, j2: The zero-based column position
+ */
+Graph.prototype.isConnected = function(i1, j1, i2, j2) {
+    if (!this._checkBound(i1, j1) ||
+        !this._checkBound(i2, j2)) return false;
+
+    var id1 = this._getId(i1, j1),
+        id2 = this._getId(i2, j2);
+
+    if (typeof this._edges[id1] === 'undefined') {
+        return false;
+    }
+    return this._edges[id1][id2];
+};
+
+/*
+ * Public member function - connect the edge of two vertices
+ *
+ * @param {Integer} i1, i2: The zero-based row position
+ * @param {Integer} j1, j2: The zero-based column position
+ */
+Graph.prototype.connect = function(i1, j1, i2, j2) {
+    if (!this._checkBound(i1, j1) ||
+        !this._checkBound(i2, j2)) return false;
+
+    var id1 = this._getId(i1, j1),
+        id2 = this._getId(i2, j2);
+
+    if (typeof this._edges[id1] === 'undefined') {
+        this._edges[id1] = {};
+    }
+    this._edges[id1][id2] = true;
+
+    return true;
+};
+
+/*
+ * Public member function - disconnect the edge of two vertices
+ *
+ * @param {Integer} i1, i2: The zero-based row position
+ * @param {Integer} j1, j2: The zero-based column position
+ */
+Graph.prototype.disconnect = function(i1, j1, i2, j2) {
+    if (!this._checkBound(i1, j1) ||
+        !this._checkBound(i2, j2)) return false;
+
+    var id1 = this._getId(i1, j1),
+        id2 = this._getId(i2, j2);
+
+    if (typeof this._edges[id1] === 'undefined') {
+        return true;
+    }
+    this._edges[id1][id2] = false;
+
+    return true;
 };
 
 //  Exports
--- test/graph_spec.js
+++ test/graph_spec.js
@@ -10,3 +10,76 @@ describe('Graph constructor', function(){
         expect(g._columnCount).toEqual(100);
     });
 });
+
+describe('Graph.prototype.insert', function(){
+    it('is a function to insert element to the Graph', function() {
+        var g = new Graph(100, 100);
+        g.insert(0, 0, 1);
+
+        expect(g._data[0][0]).toEqual(1);
+        expect(g.insert(1, 1, 2)).toBe(true);
+        expect(g.insert(-1, -1, 2)).toBe(false);
+        expect(g.insert(100, 100, 2)).toBe(false);
+        expect(g._data[1][1]).toEqual(2);
+    });
+});
+
+describe('Graph.prototype.get', function(){
+    it('is a function to get element from the Graph', function() {
+        var g = new Graph(100, 100);
+        g.insert(0, 0, 1);
+
+        expect(g.get(0, 0)).toEqual(1);
+        expect(g.get(1, 1)).toBe(undefined);
+        expect(g.get(100, 100)).toBeNull();
+    });
+});
+
+describe('Graph.prototype.rowCount and Graph.prototype.columnCount', function(){
+    it('are functions to get the counts of rows and columns from the Graph', function() {
+        var g = new Graph(100, 100);
+
+        expect(g.rowCount()).toEqual(100);
+        expect(g.columnCount()).toEqual(100);
+    });
+});
+
+describe('Graph.prototype._getId', function(){
+    it('is a function to get a id from a pair of positions', function() {
+        var g = new Graph(100, 100);
+
+        expect(g._getId(10, 12)).toEqual('1012');
+        expect(g._getId(100, 102)).toBeNull();
+    });
+});
+
+describe('Graph.prototype.isConnected', function(){
+    it('is a function to check if two pairs of positions are connected', function() {
+        var g = new Graph(100, 100);
+
+        expect(g.isConnected(0, 0, 1, 1)).toBe(false);
+        expect(g.isConnected(100, 100, 1000, 1000)).toBe(false);
+    });
+});
+
+describe('Graph.prototype.connect', function(){
+    it('is a function to connect two pairs of positions', function() {
+        var g = new Graph(100, 100);
+
+        expect(g.connect(0, 0, 1, 1)).toBe(true);
+        expect(g.isConnected(0, 0, 1, 1)).toBe(true);
+        expect(g.connect(100, 100, 1000, 1000)).toBe(false);
+    });
+});
+
+describe('Graph.prototype.disconnect', function(){
+    it('is a function to disconnect two pairs of positions', function() {
+        var g = new Graph(100, 100);
+
+        expect(g.connect(0, 0, 1, 1)).toBe(true);
+        expect(g.isConnected(0, 0, 1, 1)).toBe(true);
+        expect(g.disconnect(0, 0, 1, 1)).toBe(true);
+        expect(g.isConnected(0, 0, 1, 1)).toBe(false);
+        expect(g.disconnect(100, 100, 1000, 1000)).toBe(false);
+    });
+});
